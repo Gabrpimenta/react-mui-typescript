70c76285544ab98846e983535f8579c1b0e27335
Gabrpimenta <gabrielrpimenta@gmail.com>
2023-02-27 14:34:31 -0300

    ✨ Dark and Light Themes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,20 @@
+import Button from '@mui/material/Button';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
 
 const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
-      <Route path='/home' element={<p>Home</p>} />
+      <Route
+        path='/home'
+        element={
+          <Button variant='contained' color='primary' onClick={toggleTheme}>
+            Toggle Theme
+          </Button>
+        }
+      />
       <Route path='*' element={<Navigate to='/home' />} />
     </Routes>
   );
